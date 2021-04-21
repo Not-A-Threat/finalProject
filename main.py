10fd8bd0db3839e1e97205cbf5bbb36f061a5145
josephc2195 <chicaj1221@gmail.com>
2021-04-21 12:21:18 -0400

    update

--- main.py
+++ main.py
@@ -1,3 +1,47 @@
 import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
 import pandas as pd 
 import plotly.graph_objs as go 
+import os
+
+#loading excel file from dataset folder
+cwd = os.getcwd()
+
+multiline_df = pd.read_excel(cwd + '\Datasets\Overall_Energy.xlsx')
+'''print(multiline_df)
+'''
+multiline_df['Month'] = pd.to_datetime(multiline_df['Month'])
+trace1_multiline = go.Scatter(x=multiline_df['Month'], y=multiline_df['Total Fossil Fuels Production'], mode='lines', name='Fossil Fuel Production')
+trace2_multiline = go.Scatter(x=multiline_df['Month'], y=multiline_df['Total Renewable Energy Production'], mode='lines', name='Renewable Energy Production')
+
+data_multiline = [trace1_multiline, trace2_multiline]
+
+app = dash.Dash()
+
+app.layout = html.Div(children=[
+    html.H1(children='Python Dash',
+            style={
+                'textAlign': 'center',
+                'color': '#ef3e18'
+            }
+            ),
+    html.H1('Energy production in the United States', style={'textAlign': 'center'}),
+    html.Br(),
+    html.Br(),
+    html.Hr(style={'color': '#7FDBFF'}),
+    html.H3('H3', style={'color': '#df1e56'}),
+    html.Div('3rd div'),
+    dcc.Graph(id='graph1', 
+              figure={
+                  'data': data_multiline,
+                  'layout': go.Layout(
+                      title='Fossil Fuel production vs Renewable Energy production',
+                      xaxis={'title': 'Date'}, yaxis={'title': 'Energy Production in Quadrillion Btu'})
+                  }),
+
+    ])
+
+if __name__ == '__main__':
+    app.run_server()
