0c869cbc462d6021c0d0e997b35e2ffc92255a2c
josephc2195 <chicaj1221@gmail.com>
2021-04-22 15:27:02 -0400

    update

--- main.py
+++ main.py
@@ -14,11 +14,8 @@ state_consumption_df = pd.read_excel(cwd + '/Datasets/State_Energy_Consumption.x
 multiline_df = pd.read_excel(cwd + '/Datasets/Overall_Energy.xlsx')
 
 state_consumption_df = state_consumption_df.groupby(['State', 'Consumption', 'Rank', 'Consumption per Capita', 'Expenditures'])['Rank']
-print(state_consumption_df.count())
 #splits data into states and just energy
-#split_states = state_consumption_df['State'].str.split(' ')
-#split_energy = state_consumption_df['Consumption'].str.split(' ')
- 
+
 #making lines for the multiline chart
 multiline_df['Month'] = pd.to_datetime(multiline_df['Month'])
 trace1_multiline = go.Scatter(x=multiline_df['Month'], y=multiline_df['Total Fossil Fuels Production'], mode='lines', name='Fossil Fuel Production')
@@ -40,7 +37,7 @@ app.layout = html.Div(children=[
     html.Br(),
     html.Hr(style={'color': '#7FDBFF'}),
     html.H3('Map of the US', style={'color': '#df1e56'}),
-    html.Div('Click a State to get started:'),
+    html.Div('Click a state below to get started:'),
     html.Iframe(src="https://createaclickablemap.com/map.php?&id=102341&maplocation=false&online=true", width='1200', height='700'),
     dcc.Graph(id='graph1', 
               figure={
