7a2b90836ad571a8f220284bbc1e527d18d62266
josephc2195 <chicaj1221@gmail.com>
2021-04-22 12:32:33 -0400

    update

--- main.py
+++ main.py
@@ -6,27 +6,26 @@ import pandas as pd
 import plotly.graph_objs as go 
 import os
 
-#loading excel file from dataset folder
+#Grabs current working directory
 cwd = os.getcwd()
 
+#loads data from excel file 
 state_consumption_df = pd.read_excel(cwd + '\Datasets\State_Energy_Consumption.xls')
 multiline_df = pd.read_excel(cwd + '\Datasets\Overall_Energy.xlsx')
 
-states_by_consumption = pd.DataFrame(state_consumption_df, columns=['State'])
-split_states = states_by_consumption['State'].str.split(' ')
-
-energy_consumption = pd.DataFrame(state_consumption_df, columns=['Consumption'])
-split_energy = energy_consumption['Consumption'].str.split(' ')
-print(split_energy)
-
+#splits data into states and just energy
+split_states = state_consumption_df['State'].str.split(' ')
+split_energy = state_consumption_df['Consumption'].str.split(' ')
+ 
+#making lines for the multiline chart
 multiline_df['Month'] = pd.to_datetime(multiline_df['Month'])
 trace1_multiline = go.Scatter(x=multiline_df['Month'], y=multiline_df['Total Fossil Fuels Production'], mode='lines', name='Fossil Fuel Production')
 trace2_multiline = go.Scatter(x=multiline_df['Month'], y=multiline_df['Total Renewable Energy Production'], mode='lines', name='Renewable Energy Production')
-
 data_multiline = [trace1_multiline, trace2_multiline]
 
 app = dash.Dash()
 
+#html layout of the page
 app.layout = html.Div(children=[
     html.H1(children='Python Dash',
             style={
@@ -34,6 +33,7 @@ app.layout = html.Div(children=[
                 'color': '#ef3e18'
             }
             ),
+
     html.H1('Energy production in the United States', style={'textAlign': 'center'}),
     html.Br(),
     html.Br(),
@@ -45,9 +45,9 @@ app.layout = html.Div(children=[
                   'data': data_multiline,
                   'layout': go.Layout(
                       title='Fossil Fuel production vs Renewable Energy production',
-                      xaxis={'title': 'Date'}, yaxis={'title': 'Energy Production in Quadrillion Btu'})
-                  }),
-    ])
+                      xaxis={'title': 'Date'}, yaxis={'title': 'Energy Production in Quadrillion Btu'})       
+    }),
+])
 
 if __name__ == '__main__':
     app.run_server()
